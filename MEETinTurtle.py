afc5bf7251fae2bb666e700e238be59a2e20daa2
shirt20-meet <shirt20@meet.mit.edu>
2018-07-29 11:28:47 -0400

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+###draw the E
+
+turtle.penup()
+turtle.goto(-80,-100)
+turtle.pendown()
+
+turtle.goto(-80,-100+200)
+turtle.goto(-80+100,+100)
+
+turtle.penup()
+turtle.goto(+20,+100-100)
+turtle.pendowm()
+
+turtle.goto(+20-100,0)
+
+turtle.mainloop()
+
